34bfca073ac1a0f89049f092dae22ab815413860
Dardell <Maxgodest@gmail.com>
2022-09-04 01:36:12 +0300

    Built front changes to make Heroku deploy work

--- src/main/java/com/dardell/PhoneBookAPI/controller/EntryController.java
+++ src/main/java/com/dardell/PhoneBookAPI/controller/EntryController.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://phone-book-my-api.herokuapp.com/")
 @RestController
 @RequestMapping("/api/v1/")
 public class EntryController {
